c8418aeb8d8a6a8f8e43281a2a75c17c9cc1bd84
AetherealDev <131472446+AetherealDev@users.noreply.github.com>
2023-09-10 15:48:15 -0400

    added added routes and controllers for thoughts

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,6 @@ module.exports = {
     async getUserById(req, res) {
         try {
             const user = await User.findOne({ _id: req.params.userId })
-                .select('-__V');
 
             if(!user) {
                 return res.status(404).json({ message: 'No user with that ID' });
