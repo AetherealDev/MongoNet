4336c606676385125b67601ad5b30fd9028a2ae4
AetherealDev <131472446+AetherealDev@users.noreply.github.com>
2023-09-10 15:06:38 -0400

    user routes and controllers

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const Reaction = require('./reaction');
+const User = require('./user');
+const Thought = require('./thought');
+
+module.exports = { Reaction, User, Thought };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,22 +1,10 @@
-// /api/users
-//
-// GET all users
-//
-// GET a single user by its _id and populated thought and friend data
-//
-// POST a new user:
-//
-// // example data
-// {
-//     "username": "lernantino",
-//     "email": "lernantino@gmail.com"
-// }
-// PUT to update a user by its _id
-//
-// DELETE to remove user by its _id
-//
-// BONUS: Remove a user's associated thoughts when deleted.
-
-// Path: routes\api\userRoutes.js
-
 const router = require('express').Router();
+
+const {getAllUsers, getUserById, createUser, updateUser, deleteUser, addUserFriend, removeUserFriend} = require('../../controllers/userController');
+
+// api/users
+router.route('/').get(getAllUsers).post(createUser);
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+router.route('/:userId/friends/:friendId').post(addUserFriend).delete(removeUserFriend);
+
+module.exports = router;
