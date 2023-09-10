c9306bbe01b8b7bc3542bd63c3e546a94be70c73
AetherealDev <131472446+AetherealDev@users.noreply.github.com>
2023-09-10 13:52:26 -0400

    updated thought.js

---
+++ models/thought.js
@@ -0,0 +1,40 @@
+const {Schema, Types} = require('mongoose');
+const moment = require('moment');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // use a getter method to format the timestamp on query
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'reaction'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
