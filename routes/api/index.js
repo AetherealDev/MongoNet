5d67dcb23a849a8816530c61950c42f74300fe3c
AetherealDev <131472446+AetherealDev@users.noreply.github.com>
2023-09-10 14:05:50 -0400

    starting on routes

---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const courseRoutes = require('./courseRoutes');
+const studentRoutes = require('./studentRoutes');
+
+router.use('/courses', courseRoutes);
+router.use('/students', studentRoutes);
+
+module.exports = router;
